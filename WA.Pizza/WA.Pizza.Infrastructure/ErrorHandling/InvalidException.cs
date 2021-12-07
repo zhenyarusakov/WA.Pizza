c95a0db63613893076aa20e7027dc7f284a693aa
Eugene <Eugene.Rusakov@waveaccess.ru>
2021-12-07 19:25:10 +0300

    Create Global Error Handling

---
+++ WA.Pizza/WA.Pizza.Infrastructure/ErrorHandling/InvalidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace WA.Pizza.Infrastructure.ErrorHandling
+{
+    public class InvalidException: Exception
+    {
+        public InvalidException() : base(){ }
+        
+        public InvalidException(string message) : base(message){ }
+        
+        public InvalidException(string message, params object[] args) 
+            : base(String.Format(CultureInfo.CurrentCulture, message, args)){ }
+    }
+}
